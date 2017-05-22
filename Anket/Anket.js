a760bc5e40debf5cfd6e62d39a5095d1c2ce2440
seymalaca <seymaalaca93@gmail.com>
2017-05-23 01:01:47 +0300

    AnketUygulamasi

---
+++ Anket/Anket.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    var bilgiler, ad, soyad, telefon;
+
+
+   
+    $("#gonder").click(function () {
+       
+       
+        bilgiler = {
+            adi: document.getElementById("ad").value,
+            soyadi: document.getElementById("soyad").value,
+            adres: document.getElementById("adres").value,
+            telefon: document.getElementById("tel").value,
+            hobiler: document.getElementById("hobi").value
+
+        }
+
+       
+
+        console.log(bilgiler);
+    });
+
+
+  
+
+
+});
