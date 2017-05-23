7463058c863e84c21d5d481733f0fdf80c6830e5
seymalaca <seymaalaca93@gmail.com>
2017-05-24 00:51:42 +0300

    Modified
    -Telefon numarası girişi kontrolü sağlandı.
    -Hobiler için combobox oluşturuldu.
    -Gönderme işleminden sonra form ekranı temizleme sağlandı.

--- Anket/Anket.js
+++ Anket/Anket.js
@@ -1,27 +1,43 @@
 ﻿$(document).ready(function () {
-    var bilgiler, ad, soyad, telefon;
-
-
+    var bilgiler, ad, soyad, telefon, hobi;
+    
+    $("select")
+      .change(function () {
+          var x = document.getElementById("hobi").selectedIndex;
+          var y = document.getElementById("hobi").options;
+          
+          $("select option:selected").each(function(){
+              if (y[x].text == "Diğer")
+                  document.getElementById("digerhobi").style.visibility = "visible";
+              else
+                  document.getElementById("digerhobi").style.visibility = "hidden";
+              //$("digerhobi").show();       
+          });
+            
+    
+      })
+      .change();
    
     $("#gonder").click(function () {
-       
-       
+
+        var x = document.getElementById("hobi").selectedIndex;
+        var y = document.getElementById("hobi").options;
+
         bilgiler = {
             adi: document.getElementById("ad").value,
             soyadi: document.getElementById("soyad").value,
             adres: document.getElementById("adres").value,
             telefon: document.getElementById("tel").value,
-            hobiler: document.getElementById("hobi").value
-
+            hobiler: y[x].text
         }
 
+        if (y[x].text == "Diğer") {
+            bilgiler['hobiler'] = document.getElementById("digerhobi").value
+        }    
        
-
         console.log(bilgiler);
+        document.getElementById("anketform").reset();
     });
-
-
-  
 
 
 });
