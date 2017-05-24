fde99c1c3f43ed5a5fb3af2bd154bdf478acbbfe
seymalaca <seymaalaca93@gmail.com>
2017-05-24 15:58:49 +0300

    Düzenleme2
    Combobox checkbox ile değiştirildi.
    Tüm kullanım jQuery'e çevrildi.

--- Anket/Anket.js
+++ Anket/Anket.js
@@ -1,43 +1,49 @@
-﻿$(document).ready(function () {
+﻿(jQuery)(document).ready(function () {
     var bilgiler, ad, soyad, telefon, hobi;
-    
-    $("select")
-      .change(function () {
-          var x = document.getElementById("hobi").selectedIndex;
-          var y = document.getElementById("hobi").options;
-          
-          $("select option:selected").each(function(){
-              if (y[x].text == "Diğer")
-                  document.getElementById("digerhobi").style.visibility = "visible";
-              else
-                  document.getElementById("digerhobi").style.visibility = "hidden";
-              //$("digerhobi").show();       
-          });
-            
-    
-      })
-      .change();
-   
-    $("#gonder").click(function () {
 
-        var x = document.getElementById("hobi").selectedIndex;
-        var y = document.getElementById("hobi").options;
+    (jQuery)('#digerhobi').hide();
 
-        bilgiler = {
-            adi: document.getElementById("ad").value,
-            soyadi: document.getElementById("soyad").value,
-            adres: document.getElementById("adres").value,
-            telefon: document.getElementById("tel").value,
-            hobiler: y[x].text
+    (jQuery)("input[id='diger']").click(function () {
+        if (this.checked) {
+            (jQuery)("#digerhobi").toggle();            
+        }
+        else {
+            (jQuery)('#digerhobi').hide();
+        }
+    });
+    
+
+    (jQuery)("#gonder").click(function (event) {
+
+        if ((jQuery)('#ad').val() == "" || (jQuery)('soyad').val() == "" || (jQuery)('#tel').val() == "") {
+            alert("Yıldızlı alanlar boş bırakılamaz.");
+            return false;
         }
 
-        if (y[x].text == "Diğer") {
-            bilgiler['hobiler'] = document.getElementById("digerhobi").value
-        }    
-       
-        console.log(bilgiler);
-        document.getElementById("anketform").reset();
+        var hobi = "";
+
+       (jQuery)('input[type="checkbox"]:checked').each(function () {
+             hobi = hobi + this.value + ",";
+        });      
+                
+        if ((jQuery)("#diger").is(":checked")) {
+            
+            hobi = hobi + (jQuery)("#digerhobi").val();
+        }
+           
+        var updatedHobi = hobi.replace(",diger", "");
+        
+        bilgiler = {
+        adi: (jQuery)('#ad').val(),
+        soyadi: (jQuery)('#soyad').val(),
+        adres: (jQuery)('#adres').val(),
+        telefon: (jQuery)('#tel').val(),
+        hobiler: updatedHobi
+        }
+  
+        console.log(bilgiler);              
+
     });
 
-
+    (jQuery)("#anketform")[0].reset();
 });
