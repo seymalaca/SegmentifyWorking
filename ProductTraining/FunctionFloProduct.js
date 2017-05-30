965ac64c4ef9a1aa867986913ab9a1811d53619f
mstfbiccer <mstfbiccer@gmail.com>
2017-05-30 10:13:06 +0300

    Hata yakalama task

--- ProductTraining/FunctionFloProduct.js
+++ ProductTraining/FunctionFloProduct.js
@@ -1,4 +1,6 @@
-/* Kod içerisine hangi satırın ne iş yaptığını anlatan açıklama satırları ekleyebilir misin ? :)*/
+/* Kod içerisine hangi satırın ne iş yaptığını anlatan açıklama satırları ekleyebilir misin ? :)
+Ek olarak X bir durumda kod üzerinde hata oldu diyelim, bunu müşteriye ya da console'a basmadan nasıl yakalayabilirsin?
+*/
 function product() {
 var size=[];
 var color=[];
