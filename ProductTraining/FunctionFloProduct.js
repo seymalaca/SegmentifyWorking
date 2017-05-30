19f9806f3ad5d03917ad89514d70f60aecb1af3a
seymalaca <seymaalaca93@gmail.com>
2017-05-30 21:38:29 +0300

    Düzenleme
    inStock düzeltildi.

--- ProductTraining/FunctionFloProduct.js
+++ ProductTraining/FunctionFloProduct.js
@@ -24,7 +24,7 @@ jQuery('.opt_renk a').each(function() { //opt_renk class'Ä±nda ve a etiketi altÄ
 	'image':(jQuery)('.galleryImg').attr('src'),
 	'categories': (jQuery)('meta[name="sgm:pageCat"]').attr("content").split(">"),
 	'price': ((jQuery)('#product-item div.productPrices span.newPrice').text().trim().replace(',', '.').split(/\s+/))[0],
-	'inStock':(jQuery)(".button block bg-red sepete-ekle urun-detay-sepete-ekle sp-goal-672-c308-22-1492031008114").is(":visible"),
+	'inStock':(jQuery)(".button.block.bg-red.sepete-ekle.urun-detay-sepete-ekle").is(":visible"),
 	'brand':(jQuery)(".productTitle").text().trim(),
 	'gender':(jQuery)("td[data-attr='cinsiyet']").text().trim(),
 	'labels':(jQuery)("table[class='amlabel-table top-center']"),
